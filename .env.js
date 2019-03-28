8d72064033469562f317bdd894dcf87fbeadb61e
Gabor Kurucz <gabor.kurucz@pronovix.com>
2019-03-28 14:05:53 +0100

    Update example env file

--- .env.js
+++ .env.js
@@ -1,6 +1,6 @@
 // Make available environment specific variables.
 module.exports = {
   localThemePaths: [
-    '../web/themes/devportal/dp_zero_gravity',
+    // '../web/path/to/my/theme',
   ],
 };
